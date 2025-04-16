cbd4c38db8c11b008bc6c9e0cbb026fc1ff4995c
K-KY <kyuyoungk@naver.com>
2025-04-17 01:35:56 +0900

    Feat : 실행된 쓰레드 종료

--- app/hello.py
+++ app/hello.py
@@ -4,17 +4,31 @@ import time
 
 app = FastAPI()
 
+task = True
+
+
 @app.get("/hi")
-def greet() :
+def greet():
     return "Hello World"
 
+
 def background_task(name: str, delay: float = 1.0):
-    for i in range(10):
+    for i in range(1000):
+        if (task == False):
+            break
         time.sleep(delay)
-        print(f"[{name}] 작업 {i+1} 완료")
+        print(f"[{name}] 작업 {i + 1} 완료")
+
 
 @app.get("/start-task")
 def start_background_thread():
     thread = threading.Thread(target=background_task, args=("백그라운드 작업", 1), daemon=True)
     thread.start()
     return {"status": "작업이 백그라운드에서 시작되었습니다."}
+
+
+@app.get("/stop-task")
+def stop_background_thread():
+    global task
+    task = False
+    return {"status" : "task Stop"}
